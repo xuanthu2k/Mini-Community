4f6e8a9a26c601eeae1e802ba6765a6db1010464
xuanthu2k <xuanthuphan2k@gmail.com>
2023-02-14 21:46:26 +0700

    cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,45 +1,50 @@
-const mongoose = require('mongoose')
-const {User, Post, Comment} = require('../models/model')
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const { User, Post, Comment } = require("../models/model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 const authController = {
     // Login
-    login:async(req,res)=>{
+    login: async (req, res) => {
         try {
-            const user = await User.findOne({"username":req.body.username})
-            if(!user){
+            const user = await User.findOne({ username: req.body.username });
+            if (!user) {
                 return res.json({
                     code: 400,
-                    message: "wrong username"
-                })
+                    message: "wrong username",
+                });
             }
-            const comparePass = await bcrypt.compare(req.body.password, user.password)
-            if(!comparePass){
+            const comparePass = await bcrypt.compare(
+                req.body.password,
+                user.password
+            );
+            if (!comparePass) {
                 return res.json({
                     code: 400,
-                    message: 'wrong password'
-                })
+                    message: "wrong password",
+                });
             }
             const payload = {
                 username: user.username,
-                id: user.id
-            }
-            const token = await jwt.sign(payload,process.env.TOKEN_SECRET,{expiresIn: "1d"})
+                id: user.id,
+            };
+            const token = await jwt.sign(payload, process.env.TOKEN_SECRET, {
+                expiresIn: "1d",
+            });
             return res.json({
                 code: 200,
                 message: "Login successfully",
-                userID:user.id,
+                userID: user.id,
                 admin: user.admin,
-                token
-            })
+                token,
+            });
         } catch (error) {
             return res.json({
-                code:500,
-                error
-            })
+                code: 500,
+                error,
+            });
         }
-    }
-}
+    },
+};
 
-module.exports = authController
+module.exports = authController;
--- index.js
+++ index.js
@@ -1,34 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const bcrypt = require('bcrypt')
-const cors = require('cors')
-const jwt = require('jsonwebtoken')
-const userRoute = require('./routes/userRoute')
-const postRoute = require('./routes/postRoute')
-const commentRoute = require('./routes/commentRoute')
-const authRoute = require('./routes/authRoute')
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bcrypt = require("bcrypt");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const userRoute = require("./routes/userRoute");
+const postRoute = require("./routes/postRoute");
+const commentRoute = require("./routes/commentRoute");
+const authRoute = require("./routes/authRoute");
 
-dotenv.config()
-const app = express()
-app.use(express.json())
-app.use(cors())
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(
+    cors({
+        origin: "*",
+    })
+);
 
 //connect DB
-mongoose.connect(process.env.MONGO_URI,()=>{console.log('Ket noi db thanh cong');})
+mongoose.connect(process.env.MONGO_URI, () => {
+    console.log("Ket noi db thanh cong");
+});
 
 //Routes
-app.use('/api/user',userRoute)
-app.use('/api/post',postRoute)
-app.use('/api/comment',commentRoute)
-app.use('/api/auth',authRoute)
+app.use("/api/user", userRoute);
+app.use("/api/post", postRoute);
+app.use("/api/comment", commentRoute);
+app.use("/api/auth", authRoute);
 
-app.get('/',(req,res)=>{
+app.get("/", (req, res) => {
     res.json({
-        message: 'This link not support, please add /api/user or /api/post or /api/comment'
-    })
-})
+        message:
+            "This link not support, please add /api/user or /api/post or /api/comment",
+    });
+});
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT, () => {
     console.log(`Server runing at http://localhost:${process.env.PORT}`);
-})
+});
